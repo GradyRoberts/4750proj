f8de662963ce0454b4708188defeafdfdd918c14
Grady Roberts <gnr7aj@virginia.edu>
2020-11-11 13:29:34 -0500

    add starting code for registering user

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
+
+from register import hash_pwd, check_pwd
+
 
 app = Flask(__name__)
 
@@ -18,9 +21,24 @@ mysql = MySQL(app)
 @app.route('/')
 def index():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM Users")
+    cur.execute("""SELECT * FROM Users""")
     rv = cur.fetchall()
+    cur.close()
     return str(rv)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        hashed_password = hash_pwd(password)
+
+        cur = mysql.connection.cursor()
+        sql = """INSERT INTO Users VALUES (%s,%s,%s,%s)"""
+        cur.execute(sql, (fname, lname, email, hashed_password))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
