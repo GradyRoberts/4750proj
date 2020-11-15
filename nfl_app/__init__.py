2b3036c48bd82deecd3b4f7f55026b7a5dc1c8ad
Grady Roberts <gnr7aj@virginia.edu>
2020-11-14 19:36:24 -0500

    switch from obj config to env

--- nfl_app/__init__.py
+++ nfl_app/__init__.py
@@ -2,13 +2,20 @@
 Creates and configures the app context.
 """
 
+import os
+
+
 from flask import Flask
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_object("config.Config")
+    app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+    app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+    app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+    app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+    app.config["SECRET_KEY"] = os.urandom(24)
 
     with app.app_context():
         from . import routes
